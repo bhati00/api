15b7715ff919f2652071fe1b89c1dd28107518e5
bhati00 <princebhati074@gmail.com>
2024-04-10 00:23:00 +0530

    applied user migration

--- API/EcommerceApis/models.py
+++ API/EcommerceApis/models.py
@@ -1,3 +1,27 @@
+from django.utils import timezone
 from django.db import models
-# Create your models here.
+from .Enums.userType import UserType
+import uuid
 
+
+# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(
+        primary_key= True,
+        default= uuid.uuid4,
+        unique= True,
+        editable= False
+    )
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.EmailField()
+    password = models.TextField()
+    user_type = models.IntegerField(choices= UserType.choices(), default= UserType.BUYER)
+    is_active = models.BooleanField(default= True)
+    wallet_balance = models.FloatField()
+    created_at = models.DateTimeField(auto_now= True)
+    updated_at = models.DateTimeField(auto_now_add= True)
+    
+    def __str__(self) :
+        return self.id
+    
