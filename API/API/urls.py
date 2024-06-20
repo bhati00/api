fe556517fd41f16e0eaadce65f574799baca7ef0
bhati00 <princebhati074@gmail.com>
2024-06-20 23:51:24 +0530

    fixed issues due to dj rest auth

--- API/API/urls.py
+++ API/API/urls.py
@@ -22,10 +22,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("users.v1.urls")),
-    path('users/', include('dj_rest_auth.urls'), name="users"),
-    path('users/registration/', include('dj_rest_auth.registration.urls')),
+    path('users/', include('rest_auth.urls'), name="users"),
+    path('users/registration/', include('rest_auth.registration.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
--- API/users/v1/urls.py
+++ API/users/v1/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dj_rest_auth.registration.views import ConfirmEmailView
+from rest_auth.registration.views import ConfirmEmailView
 from . import views
 
 urlpatterns = [
