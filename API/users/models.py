60a200024483d57a8cc3fef8b498645925968d9d
bhati00 <princebhati074@gmail.com>
2024-07-28 14:04:43 +0530

    added custom user model for django auth

--- API/users/models.py
+++ API/users/models.py
@@ -1,23 +1,40 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 import uuid
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
 from .Enums.userType import UserType
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="userProfile")
-    user_guid = models.UUIDField(
-        default=uuid.uuid4,
-        unique=True,
-        editable=False
-    )
-    user_type = models.IntegerField(choices=UserType.choices(), default=UserType.BUYER)
-    wallet_balance = models.FloatField()
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
     
-    class Meta:
-        ordering = ['created_at' ]  # Corrected the ordering syntax
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+    
+    
+
+# class UserProfile(models.Model):
+#     REQUIRED_FIELDS = []
+#     USERNAME_FIELD = "user_guid"
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="userProfile")
+#     user_guid = models.UUIDField(
+#         default=uuid.uuid4,
+#         unique=True,
+#         editable=False
+#     )
+#     user_type = models.IntegerField(choices=UserType.choices(), default=UserType.BUYER)
+#     wallet_balance = models.FloatField()
+#     created_at = models.DateTimeField(auto_now=True)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+    
+#     class Meta:
+#         ordering = ['created_at' ]  # Corrected the ordering syntax
+        
+
 
--- API/users/v1/urls.py
+++ API/users/v1/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from dj_rest_auth.registration.views import ConfirmEmailView
-from . import views
+
+
 
 urlpatterns = [
-
-    path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+    
 ]
